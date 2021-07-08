469f0683a40b94aeb4486c8b025d0431b9e39f59
matthew-484 <matthew.pazhaidam@yahoo.ca>
2021-07-08 19:54:32 -0400

    new

--- src/Webpages/home.js
+++ src/Webpages/home.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import "../App.css";
+import bibleImage from "../images/bible.jpeg";
+import { LoremIpsum } from "lorem-ipsum";
+
+const lorem = new LoremIpsum({
+  wordsPerSentence: {
+    max: 10,
+    min: 4,
+  },
+});
 
 class home extends Component {
   state = {};
   render() {
     return (
-      <div className="pages">
-        <h1>Home</h1>
+      <div className="divStyles">
+        <h1 className="title">Verse of the Day</h1>
+
+        <div className="verse-div">
+          <img className="verse-image" src={bibleImage} alt="Unable to Load" />
+          <h4 className="verse">{lorem.generateSentences(5)}</h4>
+        </div>
       </div>
     );
   }
